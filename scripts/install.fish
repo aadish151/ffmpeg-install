86f0442e68c206cebb8baefc69c1df96a4f69004
Aadi C <aadishchaubal@hotmail.com>
2024-04-26 09:23:15 -0400

    update directories

--- scripts/install.fish
+++ scripts/install.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/fish
 
 sudo nala install -y nvidia-cuda-toolkit build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev clang
-git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
-cd nv-codec-headers && sudo make install && ..
-git clone https://git.ffmpeg.org/ffmpeg.git && cd ffmpeg
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git ~/nv-codec-headers
+cd ~/nv-codec-headers && sudo make install && ..
+git clone https://git.ffmpeg.org/ffmpeg.git ~/ffmpeg && cd ~/ffmpeg
 set -l ver (git branch -a | grep 'release' | tail -1 | string sub -s 18)
 echo -e '\e[32mInstalling FFmpeg \e[33m'$ver'\e[0m'
 git switch $ver
